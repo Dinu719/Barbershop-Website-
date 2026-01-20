254c79c4edc616d038ab4aa1ec57719a8e36586d
Dinu719 <yorsalem719@gmail.com>
2026-01-19 19:53:44 -0600

    Add files via upload

---
+++ Barbershop Website/Project.js
@@ -0,0 +1,24 @@
+let slideIndex = 0;
+   
+    document.addEventListener("DOMContentLoaded", showSlides)
+
+    function showSlides() {
+      let i;
+      let slides = document.querySelectorAll("div#main>div");
+      let dots = document.querySelectorAll("div span");
+      for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+      }
+      for (i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}    
+     
+      slides[slideIndex-1].style.display = "block";  
+      dots[slideIndex-1].classList.add("active");
+      setTimeout(showSlides, 3000); // Change image every 3 seconds
+    }
+
+
+
